260ee205653fde634fb46a1f15f3d3233f31b5cb
xkevin190 <kevinvelasco193@gmail.com>
2020-10-15 19:44:24 -0400

    added configuration for versions add automatic versions

--- android/keystore.sh
+++ android/keystore.sh
@@ -2,6 +2,13 @@
 
 cp gradle.properties.in gradle.properties
 
+if [ "${VERSION}" ]
+then
+  echo "COMPILATION_VERSION=${VERSION}" >> gradle.properties
+else
+  echo "COMPILATION_VERSION=1.0" >> gradle.properties
+fi
+
 echo "MYAPP_RELEASE_STORE_FILE=my-upload-key.keystore" >> gradle.properties
 echo "MYAPP_RELEASE_KEY_ALIAS=${KEYALIAS}" >> gradle.properties
 echo "MYAPP_RELEASE_STORE_PASSWORD=${STOREPASSWORD}" >> gradle.properties
