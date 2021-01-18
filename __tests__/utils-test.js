24310ca4c964c5907dd8063ca002ba8cb2808347
xkevin190 <kevinvelasco193@gmail.com>
2021-01-17 20:31:05 -0400

    fix unit test error

--- __tests__/utils-test.js
+++ __tests__/utils-test.js
@@ -3,7 +3,7 @@ import '../__Mocks__';
 
 import MockData from '../__Mocks__/dataMock';
 import {
-  hashGenerateColort, getIcon, unSelect, getPhotoBase64
+  hashGenerateColort, getIcon, unSelect, getPhotoBase64, onNotification
 } from '../src/utils/utils';
 
 describe('generate hexadecimal color', () => {
@@ -57,21 +57,21 @@ describe('function to select chat and contacts', () => {
   });
 
 
-  // test('onNotification function', () => {
-  //   const sendObject = {
-  //     toUID: 'broadcast',
-  //     msg: {
-  //       text: 'test',
-  //       typeFile: 'image',
-  //       file: 'test'
-  //     },
-  //     id: 'test',
-  //     msgID: 'test',
-  //     timestamp: new Date().getTime(),
-  //     type: 1
-  //   };
-  //   onNotification(sendObject);
-  // });
+  test('onNotification function', () => {
+    const sendObject = {
+      toUID: 'broadcast',
+      msg: {
+        text: 'test',
+        typeFile: 'image',
+        file: 'test'
+      },
+      id: 'test',
+      msgID: 'test',
+      timestamp: new Date().getTime(),
+      type: 1
+    };
+    onNotification(sendObject);
+  });
 
   test('get base64 photo function', async () => {
     const path = await getPhotoBase64('test');
