ec3eb581a9c09367c6892ef5fef4111290a7ede0
xkevin190 <kevinvelasco193@gmail.com>
2021-01-27 13:29:07 -0400

    added unit tests

--- __tests__/ChatService-test.js
+++ __tests__/ChatService-test.js
@@ -66,6 +66,11 @@ describe('test chat service component', () => {
   test('onMessage test', async () => {
     const service = getChatserviceInstance();
     await service.onMessage(JSON.stringify(mockMessage));
+    mockMessage.toUID = undefined;
+    await service.onMessage(JSON.stringify(mockMessage));
+    mockMessage.toUID = 'broadcast';
+    mockMessage.type = messageType.STATUS;
+    await service.onMessage(JSON.stringify(mockMessage));
   });
 
   afterAll(() => {
